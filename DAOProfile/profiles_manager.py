400adfb2b6d8193de5e496f04389eb3e9483c0fb
patricia.macedo@estsetubal.ips.pt <patricia.macedo@estsetubal.ips.pt>
2020-02-04 15:52:37 +0000

    create Profile classes and DAOACCESS draft

--- DAOProfile/profiles_manager.py
+++ DAOProfile/profiles_manager.py
@@ -4,8 +4,8 @@ from DAOProfile.profile import Profile
 class Profiles_Manager:
     def __init__(self,connection):
         dao=DAO_profiles(connection)
-        profiles_list=dao.load_profiles() #change to hashMap
-        activate_profile=None  #profile activated
+        profiles_list=dao.load_profiles()
+        activate_profile = None
 
-     def activateProfile(self,id_profile):
+    def activateProfile(self,id_profile):
          activate_profile=id_profile #change to hashmap
