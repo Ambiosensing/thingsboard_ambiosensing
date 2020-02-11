c63815f4199fed3232f0941ff32f5ad9ff7feb5f
faf <faf@git54385438>
2020-02-11 14:06:46 +0000

    Classes building e space 2

--- DAOAmbiosensing/space.py
+++ DAOAmbiosensing/space.py
@@ -1,6 +1,18 @@
 class Space:
 
     def __init__(self, id_space, name, area, occupation_type):
-        self.id_building = id_building
+        self.id_space = id_space
         self.name = name
+        self.area = area
+        self.occupation_type = occupation_type
         # self.id_thingsboard = id_thingsboard
+        self.profiles = []
+
+    def add_profile(self, profile):
+        self.profiles.append(profile)
+
+    def toString(self):
+        return "name " + self.name + " id " + self.id_space + \
+               "area " + self.area + "occupation type " + self.occupation_type + \
+               "profiles " + self.profiles
+
