9852b2eedee69e28c0d0c154ecdd290c8ee47117
patricia.macedo@estsetubal.ips.pt <patricia.macedo@estsetubal.ips.pt>
2020-02-11 16:12:42 +0000

    create Profile classes and DAOACCESS draft

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mysql_database.python_database_modules import mysql_auth_controller as mac
+from DAOAmbiosensing.DAO_ambiosensing import DAO_ambiosensing
+from DAOAmbiosensing.profile import Profile
 
 
 def __main__():
@@ -12,22 +14,7 @@ def __main__():
     #dao.create_space(space)
     print("Done!")
 
-class car:
-    matricula = None
-    __cor = None
-    idade = 0
 
-    def __init__(self, matricula, idade, cor="Branco"):
-        self.matricula = matricula
-        self.idade = idade
-        self.cor = cor
-
-    def start(self):
-        print("Started")
-
-
-class camiao(car):
-    pass
 
 if __name__ == "__main__":
     __main__()
