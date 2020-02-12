4eb69b1d2b0f1b7428552513e907b725a0dfb8cd
faf <Faf@git54385438>
2020-02-12 23:30:06 +0000

    space class

--- DAOAmbiosensing/space.py
+++ DAOAmbiosensing/space.py
@@ -1,20 +1,28 @@
 class Space:
 
-    def __init__(self, name, area, occupation_type, building, id_space=None):
+    def __init__(self, name, area, occupation_type, building, id_space=None, id_thingsboard=None):
         self.id_space = id_space
         self.name = name
         self.area = area
         self.occupation_type = occupation_type
-        # self.id_thingsboard = id_thingsboard
+        self.id_thingsboard = id_thingsboard
         self.profiles = []
+        self.devices = []
+        self.environment_vars = []
         self.building = building
 
     def add_profile(self, profile):
         self.profiles.append(profile)
 
-    def toString(self):
+    def add_device(self, device):
+        self.devices.append(device)
 
-        return " name:" + self.name + " id:" + str(self.id_space) + \
-               " area:" + str(self.area) + " occupation type:" + self.occupation_type
+    def add_environment_var(self, environment_var):
+        self.environment_vars.append(environment_var)
+
+    def to_string(self):
+        return "Space id: " + str(self.id_space) + " Space id TB: " + str(self.id_thingsboard) +\
+               " Name: " + self.name + " Area:" + str(self.area) +\
+               " Occupation type:" + self.occupation_type + " Building id: " + self.building.id_building
 
 
