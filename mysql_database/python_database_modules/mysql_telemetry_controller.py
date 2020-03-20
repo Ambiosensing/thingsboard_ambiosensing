e1caacbc1f569408a2df11e8f49eac6db22ec363
rdlalmeida <rdlalmeida@gmail.com>
2020-03-20 19:49:30 +0000

    Humm... weird... I've developed a small Python module to retrieve the status of the Relays in the RPi relay board and another to set them ON and OFF. Strangely, the most complex and error prone method works but the simpler one doesn't... need to debug this but its too late already. Has to be tomorrow...

--- mysql_database/python_database_modules/mysql_telemetry_controller.py
+++ mysql_database/python_database_modules/mysql_telemetry_controller.py
@@ -1,1 +1,2 @@
 """ Place holder for methods related to the interface between the MySQL database (MySQL internal Ambiosensing database) and the data obtained from service calls placed to the API group telemetry-controller"""
+
