649d3c5ca5ff495c904f0be9b83fe51755c2de49
Ricardo Almeida <rdlalmeida@gmail.com>
2020-04-21 15:59:30 +0100

    Added a test method

--- __tester__.py
+++ __tester__.py
@@ -1,18 +1,25 @@
 auth_ctrl = False
 ent_rel = False
 asset_ctrl = False
-mysql_test = False
 mysql_device = False
 attr_device = False
 rpc_one_way = False
-tb_telemetry = True
+tb_telemetry = False
 db_add = False
 sql_gen = False
 date_converter = False
 table_creator = False
+device_types = True
 
 
 def __main__():
+    if device_types:
+        from ThingsBoard_REST_API import tb_device_controller
+
+        result = tb_device_controller.getDeviceTypes()
+
+        print(str(result.text))
+
     if date_converter:
         from mysql_database.python_database_modules import mysql_utils
         ts1 = 1582147684275
@@ -21,7 +28,6 @@ def __main__():
 
     if attr_device:
         from ThingsBoard_REST_API import tb_telemetry_controller as ttc
-        import proj_config
         device_name = "Multi-sensor device"
 
         resp = ttc.getAttributes(deviceName=device_name)
@@ -52,10 +58,9 @@ def __main__():
 
     if tb_telemetry:
         from ThingsBoard_REST_API import tb_telemetry_controller as ttc
-        import datetime
         device_name = 'Rasp_00040'
-        end_time = datetime.datetime(2020, 2, 19, 21, 28, 4)
-        time_interval = int(datetime.timedelta(hours=24).total_seconds())
+        # end_time = datetime.datetime(2020, 2, 19, 21, 28, 4)
+        # time_interval = int(datetime.timedelta(hours=24).total_seconds())
 
         # result = ttc.getTimeseries(device_name=device_name, end_time=end_time, time_interval=time_interval)
         result = ttc.getLatestTimeseries(device_name=device_name)
@@ -88,33 +93,14 @@ def __main__():
         response = trc.handleTwoWayDeviceRPCRequest(deviceId=deviceId, remote_method=method, param_dict=param_dict)
         print("RPC command returned HTTP " + str(response))
 
-    if mysql_test:
-        from mysql_database.python_database_modules import mysql_utils
-        import _mysql_connector
-        table_name = 'tb_tenant_assets'
-        database_name = "ambiosensing_thingsboard"
-
-        cnx = mysql_utils.connect_db(database_name)
-        change_cursor = cnx.cursor(buffered=True)
-
-        # sql_insert = """INSERT INTO tb_tenant_assets (entityType, id, createdTime, description, tenantId, customerId, name, type) VALUES ('ASSET', 'efa6d2d0-0ad9-11ea-8001-3975f352e04e', '2019-11-19 14:36:13', 'A test building that houses two
-        # Thermometer devices', '863ae890-0ad9-11ea-8001-3975f352e04e', '3360f510-0fde-11ea-b852-37722cd69450', 'Building A', 'building');"""
-
-        sql_update = """UPDATE tb_tenant_assets SET entityType = 'ASSET', id = 'efa6d2d0-0ad9-11ea-8001-3975f352e04e', createdTime = '2019-11-19 14:36:13', description = 'A test building that houses two Thermometer devices', tenantId = '863ae890-0ad9-11ea-8001-3975f352e04e', customerId = '3360f510-0fde-11ea-b852-37722cd69450', name = 'Building A', type = 'building';"""
-
-        try:
-            change_cursor = mysql_utils.run_sql_statement(cursor=change_cursor, sql_statement=sql_update, data_tuple=())
-        except mysql_utils.MySQLDatabaseException as me:
-            print("Got a freakin duplicated result: " + me.message)
-
-        print("OK")
-
     if mysql_device:
         from mysql_database.python_database_modules import mysql_device_controller as mdc
 
         # mdc.update_devices_table()
         device_name = "Rotating System"
         result = mdc.get_device_credentials(device_name=device_name)
+
+        print(str(result))
 
     if table_creator:
         from mysql_database.python_database_modules import mysql_utils
